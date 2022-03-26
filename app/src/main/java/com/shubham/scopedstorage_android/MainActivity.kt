9efc55a8c036cd9e4632906a6ecaca04f01c855f
Shubham Shinde <shindesshubham96@gmail.com>
2022-03-26 15:55:59 +0530

    initial commit

---
+++ app/src/main/java/com/shubham/scopedstorage_android/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.shubham.scopedstorage_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
